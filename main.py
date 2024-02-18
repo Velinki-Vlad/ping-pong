97307abe227375dbf888bf708e2f7c73cf38b53f
Velinki-Vlad <160403558+Velinki-Vlad@users.noreply.github.com>
2024-02-18 16:47:28 +0300

    V.1.0
    Добавили фон/
    Added a background

---
+++ main.py
@@ -0,0 +1,50 @@
+from random import *
+from pygame import *
+from time import time as timer
+window = display.set_mode((700,500))
+
+win_witdh = 700
+win_height = 500
+
+finish = False
+
+font.init()
+font1 = font.SysFont(None, 36)
+font2 = font.SysFont(None, 75)
+
+background = image.load('background_ping-pong.jpg')
+background = transform.scale(background, (700,500))
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x,player_y,size_x,size_y,player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image,(self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys [K_LEFT] and self.rect.x > 5:
+            self.rect.x -= self.speed
+        if keys [K_RIGHT] and self.rect.x < 620:
+            self.rect.x += self.speed
+
+clock = time.Clock()
+run = True
+while run:
+    for e in event.get():
+        if e.type == QUIT:
+            run = False
+    if not finish:
+        
+        window.blit(background, (0, 0))
+        #player.update()
+        #player.reset()
+
+    display.update()
+    clock.tick(40)
