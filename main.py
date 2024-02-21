1168a7f6b1e029e8e52e4f62850c7a0d35a82dda
Velinki-Vlad <160403558+Velinki-Vlad@users.noreply.github.com>
2024-02-21 18:11:57 +0300

    V.1.2
    Добавлена функция управлять ракетками 1ракетка(W,S) \ 2ракетка(⬆️,⬇️)

--- main.py
+++ main.py
@@ -27,11 +27,17 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image,(self.rect.x, self.rect.y))
 
 class Player(GameSprite):
-    def update(self):
+    def update1(self):
         keys = key.get_pressed()
         if keys [K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys [K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
+    def update2(self):
+        keys = key.get_pressed()
+        if keys [K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys [K_DOWN] and self.rect.y < 350:
             self.rect.y += self.speed
 
 racket1 = Player('racket.png', 30, 200, 50, 150,4)
@@ -48,9 +54,9 @@ while run:
         
         window.blit(background, (0, 0))
         
-        racket1.update()
+        racket1.update1()
         racket1.reset()
-        racket2.update()
+        racket2.update2()
         racket2.reset()
         
     display.update()
